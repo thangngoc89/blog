60fe64b77cec7e8c01d2b25cc78a3fb6e4599588
Khoa Nguyen <khoa02031995@gmail.com>
2016-01-24 22:55:11 +0700

    chore: Add github pages deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+import ghPages from 'gh-pages'
+import config from '../config'
+
+const paths = config.utils_paths
+
+ghPages.publish(paths.dist(), {
+  branch: 'master',
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thangngoc89/thangngoc89.github.io.git',
+  silent: true
+}, (err) => {
+  if (err) {
+    console.error(err)
+  }
+})
