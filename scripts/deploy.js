41d233ba36175c1df0d13ab5fdf3b267fd9aeb55
Khoa Nguyen <khoa02031995@gmail.com>
2016-01-24 22:59:26 +0700

    deploy: Run without silent

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,6 @@ const paths = config.utils_paths
 ghPages.publish(paths.dist(), {
   branch: 'master',
   repo: 'https://' + process.env.GH_TOKEN + '@github.com/thangngoc89/thangngoc89.github.io.git',
-  silent: true
 }, (err) => {
   if (err) {
     console.error(err)
