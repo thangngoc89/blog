26c8acb0995083f3f1cb3cdb6ce43cb8aeb2857b
Khoa Nguyen <khoa02031995@gmail.com>
2016-02-13 18:54:58 +0700

    feat(build): Add travis as builder

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ const dist = join(__dirname, '../dist')
 
 ghPages.publish(dist, {
   branch: 'master',
-  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thangngoc89/thangngoc89.github.io.git',
+  repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/thangngoc89/thangngoc89.github.io.git',
 }, (err) => {
   if (err) {
     console.error(err)
