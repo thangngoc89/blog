37a7fa80112b51474958284ddf31a741f1678611
Khoa Nguyen <khoa02031995@gmail.com>
2016-02-20 18:55:01 +0700

    design: Debut github edit link

--- web_modules/components/GitHubEditLink/GitHubEditLink.js
+++ web_modules/components/GitHubEditLink/GitHubEditLink.js
@@ -1,19 +1,17 @@
 import React, { PropTypes } from 'react'
+import styles from './GitHubEditLink.scss'
 
 const GitHubEditLink = ({ baseUrl, fileName }) => {
   const url = baseUrl + fileName
 
   return (
-    <div>
-      <hr />
-      <p>
-        Bài viết sai chính tả? Có điểm chưa rõ ràng?
-        {'  '}
-        <a href={url} target='_blank'>
-          Sửa bài viết trên Github
-        </a>
-      </p>
-    </div>
+    <p className={styles.text}>
+      Bài viết sai chính tả? Có điểm chưa rõ ràng?
+      {'  '}
+      <a href={url} target='_blank'>
+        Sửa bài viết trên Github
+      </a>
+    </p>
   )
 }
 
