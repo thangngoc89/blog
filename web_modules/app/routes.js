99046dc2dec4a8b7e521446b32bfd73ded9dafa2
Khoa Nguyen <khoa02031995@gmail.com>
2016-02-20 14:55:51 +0700

    Intercerpt link from markdown to trigger pushState

--- web_modules/app/routes.js
+++ web_modules/app/routes.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Route } from 'react-router'
+import { Route, browserHistory } from 'react-router'
 import PageContainer from 'statinamic/lib/PageContainer'
+import catchLinks from 'catch-links'
 
 import '../styles/global.styles'
 // components
 import LayoutContainer from '../LayoutContainer'
 
+if (typeof window !== 'undefined') {
+  catchLinks(window, (href) => {
+    browserHistory.push(href)
+  })
+}
 // routes
 export default (
   <Route component={LayoutContainer}>
