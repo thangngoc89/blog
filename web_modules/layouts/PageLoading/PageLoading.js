4ef024d2d8cc4075ff312167dba12acd3883d2f4
Khoa Nguyen <khoa02031995@gmail.com>
2016-02-21 02:31:42 +0700

    design: Simple page loading

--- web_modules/layouts/PageLoading/PageLoading.js
+++ web_modules/layouts/PageLoading/PageLoading.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import styles from './PageLoading.scss'
 
-// http://codepen.io/supah/pen/JYgqRK
-const Loading = () => {
+const PageLoading = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.ball1}></div>
-      <div className={styles.ball2}></div>
-      <div className={styles.ball3}></div>
-    </div>
+    <span>Loading ... </span>
   )
 }
 
-export default Loading
+export default PageLoading
