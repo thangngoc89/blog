7e76cf0b5ad18aff19f6f5f02abef37011d3f166
Khoa Nguyen <hi@khoanguyen.me>
2016-09-16 16:45:09 +0700

    Add GA, fix CNAME

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -5,23 +5,25 @@ import "./sass/main.scss"
 import DefaultHeadMeta from "./components/DefaultHeadMeta"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Ga from "./components/Ga"
 
 export default class AppContainer extends Component {
 
   static propTypes = {
     children: PropTypes.oneOfType([ PropTypes.array, PropTypes.object ]),
+    params: PropTypes.object,
   };
 
   render() {
     return (
-      <div>
+      <Ga params={ this.props.params }>
         <DefaultHeadMeta />
         <Header />
         <div className="content">
           { this.props.children }
         </div>
         <Footer />
-      </div>
+      </Ga>
     )
   }
 }
