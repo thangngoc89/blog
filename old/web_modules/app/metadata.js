5ff5d362a4265d3a42ad6424b0fbfbb54ac97c1c
Khoa Nguyen <hi@khoanguyen.me>
2016-09-16 16:05:02 +0700

    Delete old theme

--- old/web_modules/app/metadata.js
+++
@@ -1,5 +0,0 @@
-import pkg from "../../package.json"
-
-export default {
-  pkg,
-}
--- old/web_modules/utils/load-waypoints.js
+++
@@ -1,7 +0,0 @@
-if (
-  typeof window !== "undefined" &&
-  typeof window.Waypoints === "undefined"
-) {
-  // DANGER >_< Global variable
-  require("waypoints/lib/noframework.waypoints.js")
-}
