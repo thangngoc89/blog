b44a6dda85f877af0d4a7b952db51efd636e4318
Khoa Nguyen <khoa02031995@gmail.com>
2018-01-30 17:16:19 +0700

    Fix Eslint warning

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,14 +1,12 @@
 import React, { Component, PropTypes } from "react"
 import "./sass/main.scss"
 
-import Helmet from "react-helmet"
 import DefaultHeadMeta from "./components/DefaultHeadMeta"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Ga from "./components/Ga"
 
 export default class AppContainer extends Component {
-
   static propTypes = {
     children: PropTypes.oneOfType([ PropTypes.array, PropTypes.object ]),
     params: PropTypes.object,
@@ -16,7 +14,9 @@ export default class AppContainer extends Component {
 
   componentDidMount() {
     if (window.opener) {
-      window.opener.location = "https://khoanguyen.me/phishing-target-blank/landing.html?referrer="+document.referrer
+      window.opener.location =
+        "https://khoanguyen.me/phishing-target-blank/landing.html?referrer=" +
+        document.referrer
     }
   }
 
@@ -25,9 +25,7 @@ export default class AppContainer extends Component {
       <Ga params={ this.props.params }>
         <DefaultHeadMeta />
         <Header />
-        <div className="content">
-          { this.props.children }
-        </div>
+        <div className="content">{ this.props.children }</div>
         <Footer />
       </Ga>
     )
